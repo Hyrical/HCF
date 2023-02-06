5ed87b52e2cb8f4d550ddcdae5f243a7039edec4
Nopock <nathan@weisz.org>
2023-02-05 16:10:00 -0800

    Lives cmd

--- src/main/kotlin/org/hyrical/hcf/profile/commands/LivesCommand.kt
+++ src/main/kotlin/org/hyrical/hcf/profile/commands/LivesCommand.kt
@@ -30,12 +30,39 @@ object LivesCommand : BaseCommand() {
             ProfileService.getProfile(player.uniqueId)
         } else {
             ProfileService.getProfile(target.uniqueId)
-        }
+        } ?: return
 
         if (target == null) {
-            player.sendMessage(translate("&cYou have &e${targetProfile?.soulboundLives!! + targetProfile?.friendLives!!} &clives."))
+            player.sendMessage(translate("&cYou have &e${targetProfile.lives} &clives."))
         } else {
-            player.sendMessage(translate("&c${target.name} has &e${targetProfile?.lives} &clives."))
+            player.sendMessage(translate("&ePlayer &c${target.name} &ehas &c${targetProfile.lives} &elives."))
         }
     }
+
+    @Subcommand("send")
+    fun send(player: Player, @Name("target") target: Player, @Name("amount") amount: Int) {
+        if (amount <= 0) {
+            player.sendMessage(translate("&cYou cannot send a negative amount of lives."))
+            return
+        }
+
+        val profile = ProfileService.getProfile(player.uniqueId) ?: return
+        val targetProfile = ProfileService.getProfile(target.uniqueId) ?: return
+
+        if (profile.friendLives < amount) {
+            player.sendMessage(translate("&cYou do not have enough lives to send."))
+            return
+        }
+
+        profile.friendLives -= amount
+        targetProfile.friendLives += amount
+
+        player.sendMessage(translate("&eYou have sent &c$amount &elives to &c${target.name}&e."))
+        target.sendMessage(translate("&eYou have received &c$amount &elives from &c${player.name}&e."))
+
+        ProfileService.save(profile)
+        ProfileService.save(targetProfile)
+    }
+
+    // TODO: rewvive 
 }
