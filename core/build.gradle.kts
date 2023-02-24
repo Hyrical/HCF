2d9d419385fdd38e50883e33642a3e02aaf36534
Nopox <78834703+Nopock@users.noreply.github.com>
2023-02-24 09:11:26 -0800

    Update build.gradle.kts
    Signed-off-by: Nopox <78834703+Nopock@users.noreply.github.com>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,24 +1,23 @@
 dependencies {
-    implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
+    implementation(libs.acf.paper)
 
-    implementation("com.github.Nopock:Store:4.23")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    implementation(libs.store)
+    implementation(libs.kotlin.reflect)
 
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("org.reflections:reflections:0.10.2")
-    implementation("com.google.guava:guava:31.1-jre")
-    implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("com.eatthepath:fast-uuid:0.2.0")
-    implementation("org.mongodb:mongo-java-driver:3.12.12")
-    implementation("com.github.ThatKawaiiSam:Assemble:7b446313ba")
-    implementation("com.github.cryptomorin:XSeries:9.3.0") { isTransitive = false }
+    implementation(libs.apache.commons)
+    implementation(libs.reflections)
+    implementation(libs.google.guava)
+    implementation(libs.apache.commons.collections)
+    implementation(libs.fast.uuid)
+    implementation(libs.mongo.java.driver)
+    implementation(libs.xseries) { isTransitive = false }
 
-    implementation("com.github.mkremins:fanciful:ed870f3a19")
+    implementation(libs.fanciful)
 
     // Include all libs sub folders too
     compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("**/*.jar"))))
 
     implementation(project(":core-api"))
 
-    implementation("com.github.LunarClient:BukkitAPI-NetHandler:fab9915b0d")
+    implementation(libs.lunar.client.api)
 }
