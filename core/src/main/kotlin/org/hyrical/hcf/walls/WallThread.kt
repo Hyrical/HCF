3840fc771261ad0ff70c566b3c93c3296557853a
Nopock <nathan@weisz.org>
2023-02-27 16:48:23 -0800

    frick dick rick
    frickasd

--- core/src/main/kotlin/org/hyrical/hcf/walls/WallThread.kt
+++ core/src/main/kotlin/org/hyrical/hcf/walls/WallThread.kt
@@ -73,8 +73,8 @@ class WallThread : Thread("Wall Thread") {
     private fun getTeamsAndClaimsNearLocation(location: Location): List<Pair<Team, Cuboid>> {
         val result = mutableListOf<Pair<Team, Cuboid>>()
 
-        TeamManager.cache.values.parallelStream().forEach { team ->
-            team.claims.parallelStream().forEach { cuboid ->
+        for (team in TeamManager.cache.values) {
+            for (cuboid in team.claims) {
                 val cuboidLocation = Location(cuboid.world, cuboid.lowerX.toDouble(), 0.0, cuboid.lowerZ.toDouble())
                 val locationWithoutY = Location(location.world, location.x, 0.0, location.z)
                 val distanceSquared = cuboidLocation.distanceSquared(locationWithoutY)
@@ -86,6 +86,7 @@ class WallThread : Thread("Wall Thread") {
 
         return result
     }
+
 
 
     private fun clearBlocks(player: Player) {
@@ -121,7 +122,7 @@ class WallThread : Thread("Wall Thread") {
                 if (check.world!!.isChunkLoaded(check.blockX shr 4, check.blockZ shr 4)
                     && check.block.type.isTransparent && check.distanceSquared(onPlayerY) < 64) {
                     player.sendBlockChange(check, XMaterial.RED_STAINED_GLASS.parseMaterial()!!, 14) // Red stained glass
-                    cachedLocations[player.uniqueId]?.set(check, System.currentTimeMillis() + 4000L) // The time the glass will stay for if the player walks away
+                    cachedLocations[player.uniqueId]!![check] = System.currentTimeMillis() + 4000L // The time the glass will stay for if the player walks away
                 }
             }
         }
