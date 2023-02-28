d9eb3582bab4dcdcd69d59387677d9b95dbb7412
Nopock <nathan@weisz.org>
2023-02-27 17:43:20 -0800

    cock balls

--- core/src/main/kotlin/org/hyrical/hcf/walls/WallThread.kt
+++ core/src/main/kotlin/org/hyrical/hcf/walls/WallThread.kt
@@ -20,26 +20,19 @@ class WallThread : Thread("Wall Thread") {
     override fun run() {
         while (true) {
             sleep(250L)
-            tick()
+            showBordersToPlayers()
         }
     }
 
-    private fun tick() {
+    fun showBordersToPlayers() {
         for (player in PluginUtils.getOnlinePlayers()) {
-            if (!cachedLocations.containsKey(player.uniqueId)) {
-                cachedLocations[player.uniqueId] = mutableMapOf()
-            }
             val claims = LinkedList<Cuboid>()
-            for ((team, cuboid) in getTeamsAndClaimsNearLocation(player.location)) {
-                if (cuboid.contains(player.location)) continue
-
-                println("I HIT THE CUBIOD WITH THE SHOE AND HIT THE BIG BLACK BOO")
-
+            val playerLocation = player.location
+            for ((team, cuboid) in getTeamsAndClaimsNearLocation(playerLocation)) {
+                if (cuboid.contains(playerLocation)) continue
                 if (team.leader == null) {
                     if (hasFlag(team, Flag.SPAWN) && CombatTimer.hasTimer(player)) {
                         claims.add(cuboid)
-                        println("whby u built so goofy my nig")
-                        continue
                     } else if (hasFlag(team, Flag.KOTH) && false/* Check for pvp timer here */) {
 
                     }
@@ -48,23 +41,18 @@ class WallThread : Thread("Wall Thread") {
                 }
             }
 
-            println(claims.size)
-
-            if (claims.size == 0) {
-                // clear the walls we went them already
-                clearBlocks(player)
-                println("I cleared ther shit annd hit the nig")
+            if (claims.isEmpty()) {
+                clearBordersForPlayer(player)
                 continue
             }
 
-            val bordersIterator: Iterator<Map.Entry<Location, Long>> = cachedLocations[player.uniqueId]!!.iterator()
-
+            val bordersIterator = cachedLocations.getOrPut(player.uniqueId) { mutableMapOf() }.iterator()
             while (bordersIterator.hasNext()) {
                 val (loc, value) = bordersIterator.next()
                 if (System.currentTimeMillis() >= value) {
-                    if (!loc.world!!.isChunkLoaded(loc.blockX shr 4, loc.blockZ shr 4)) continue
-                    val block: Block = loc.block
-                    player.sendBlockChange(loc, block.type, block.data);
+                    if (!loc.chunk.isLoaded) continue
+                    player.sendBlockChange(loc, loc.block.type, loc.block.data)
+                    bordersIterator.remove()
                 }
             }
 
@@ -76,41 +64,34 @@ class WallThread : Thread("Wall Thread") {
 
     private fun getTeamsAndClaimsNearLocation(location: Location): Map<Team, Cuboid> {
         val result = mutableMapOf<Team, Cuboid>()
-
+        val locationWithoutY = Location(location.world, location.x, 0.0, location.z)
         for (team in TeamManager.cache.values) {
             for (cuboid in team.claims) {
                 val cuboidLocation = Location(cuboid.world, cuboid.lowerX.toDouble(), 0.0, cuboid.lowerZ.toDouble())
-                val locationWithoutY = Location(location.world, location.x, 0.0, location.z)
                 val distanceSquared = cuboidLocation.distanceSquared(locationWithoutY)
                 if (distanceSquared <= 64) { // squared value of 8 blocks
                     result[team] = cuboid
                 }
             }
         }
-
         return result
     }
 
-    private fun clearBlocks(player: Player) {
-        if (!cachedLocations.containsKey(player.uniqueId)) return
-
-        for (location in cachedLocations[player.uniqueId]!!.keys) {
-            if (!location.world!!.isChunkLoaded(location.blockX shr 4, location.blockZ shr 4)) continue
-            val block = location.block
-            player.sendBlockChange(location, block.type, block.data)
+    private fun clearBordersForPlayer(player: Player) {
+        cachedLocations.remove(player.uniqueId)?.keys?.forEach {
+            if (it.chunk.isLoaded) {
+                player.sendBlockChange(it, it.block.type, it.block.data)
+            }
         }
-
-        cachedLocations.remove(player.uniqueId)
     }
-
 
     private fun hasFlag(team: Team, flag: Flag): Boolean {
         return team.factionType.contains(flag)
     }
 
     private fun sendClaimToPlayer(player: Player, claim: Cuboid) {
-        // This gets us all the coordinates on the outside of the claim.
-        for (coordinate in claim.getCoordinatesOnEdge()) {
+        val edgeCoordinates = claim.getCoordinatesOnEdge()
+        for (coordinate in edgeCoordinates) {
             val onPlayerY = Location(player.world, coordinate.first.toDouble(), player.location.y, coordinate.second.toDouble())
 
             // Ignore an entire pillar if the block closest to the player is further than the max distance (none of the others will be close enough, either)
@@ -120,14 +101,14 @@ class WallThread : Thread("Wall Thread") {
 
             for (i in -4..4) {
                 val check = onPlayerY.clone().add(0.0, i.toDouble(), 0.0)
-
-                if (check.world!!.isChunkLoaded(check.blockX shr 4, check.blockZ shr 4)
-                    && check.block.type.isTransparent && check.distanceSquared(onPlayerY) < 64) {
+                if (!check.block.type.isTransparent) continue
+                if (check.world!!.isChunkLoaded(check.blockX shr 4, check.blockZ shr 4) && check.distanceSquared(onPlayerY) < 64) {
                     player.sendBlockChange(check, XMaterial.RED_STAINED_GLASS.parseMaterial()!!, 14) // Red stained glass
-                    cachedLocations[player.uniqueId]!![check] = System.currentTimeMillis() + 4000L // The time the glass will stay for if the player walks away
+                    cachedLocations.computeIfAbsent(player.uniqueId) { mutableMapOf() }[check] = System.currentTimeMillis() + 4000L // The time the glass will stay for if the player walks away
                 }
             }
         }
     }
+
 
 }
